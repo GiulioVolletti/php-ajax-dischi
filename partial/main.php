b96178acf10762c8b3053be50224221607c1c021
Giulio <giuliovolletti@hotmail.com>
2021-01-22 16:17:36 +0100

    commit 2

--- partial/main.php
+++ partial/main.php
@@ -1,3 +1,46 @@
 <main>
-  
+  <div class="container">
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+    <div class="container-albums">
+      <img src="" alt="cover">
+      <h3>nome album</h3>
+      <p>autore</p>
+      <p>anno</p>
+    </div>
+  </div>
 </main>
