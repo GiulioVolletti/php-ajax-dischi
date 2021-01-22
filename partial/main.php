3a22a7a077a47e9e9b076768d738fb8e6f84f3f6
Giulio <giuliovolletti@hotmail.com>
2021-01-22 16:48:51 +0100

    commit 3

--- partial/main.php
+++ partial/main.php
@@ -1,46 +1,15 @@
 <main>
   <div class="container">
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
-    <div class="container-albums">
-      <img src="" alt="cover">
-      <h3>nome album</h3>
-      <p>autore</p>
-      <p>anno</p>
-    </div>
+    <?php foreach ($albums as $album) { ?>
+
+      <div class="container-albums">
+        <img src=<?= $album["poster"] ?> alt="cover">
+        <h3><?= $album["title"] ?></h3>
+        <div>          
+          <p><?= $album["author"] ?></p>
+          <p><?= $album["genre"] ?></p>
+        </div>
+      </div>
+    <?php } ?>
   </div>
 </main>
