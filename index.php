dbd2ea530f5a976871d0feda73f209e5b737cb70
Giulio <giuliovolletti@hotmail.com>
2021-01-22 15:58:00 +0100

    commit 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Php Ajax Dischi</title>
+  </head>
+  <body>
+    <div id="app-container">
+      <?php
+      include __DIR__ . '/partial/header.php';
+      include __DIR__ . '/partial/main.php';
+      ?>
+    </div>
+  </body>
+</html>
